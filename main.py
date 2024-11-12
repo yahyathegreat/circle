bd0c7d121ea02d77d5c096497df72f1f6cd8e732
yahyathegreat <yahyamardan1234@gmail.com>
2024-11-12 18:28:35 +0500

    digits

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import math
 
 radius = float(input("Enter radius: "))
 print("Circumference:", 2 * math.pi * radius)
+number = input("Enter a number: ")
+num_digits = len(number) if number.isdigit() else len(number) - 1  
+print("Total digits:", num_digits)
